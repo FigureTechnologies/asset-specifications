7499bd830b934b6cd44845967c51aa00d70b9eb1
Valerie Wagner <vwagner@figure.com>
2022-03-22 14:51:02 -0600

    Refactor build structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,25 @@ buildscript {
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    id("com.google.protobuf") version "0.8.18"
-    id("com.github.marcoferrer.kroto-plus") version "0.6.1"
     `java-library`
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
-repositories {
-    mavenCentral()
+val projectVersion = project.property("version")?.takeIf { it != "unspecified" } ?: "1.0-SNAPSHOT"
+
+
+allprojects {
+    group = "io.provenance.spec"
+    version = projectVersion
+
+    repositories {
+        mavenCentral()
+    }
 }
+
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -36,90 +44,11 @@ java {
     withJavadocJar()
 }
 
-val projectVersion = project.property("version")?.takeIf { it != "unspecified" } ?: "1.0-SNAPSHOT"
 
-object Versions {
-    const val Protobuf = "3.19.1"
-    const val Provenance = "1.8.0-rc10"
-
-    // Test dependencies
-    const val JunitJupiter = "5.8.2"
-
-    // Plugins
-    const val Kotlin = "1.5.0"
-    const val ProtobufPlugin = "0.8.18"
-}
-dependencies {
-
-//    implementation("io.provenance.pbc:pbc-proto:${Versions.Provenance}")
-    implementation("io.provenance:proto-kotlin:${Versions.Provenance}")
-    implementation("io.provenance.scope:util:0.4.9")
-
-    implementation("com.google.protobuf:protobuf-java:${Versions.Protobuf}")
-    implementation("com.google.protobuf:protobuf-java-util:${Versions.Protobuf}")
-    implementation("javax.annotation:javax.annotation-api:1.3.1")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.JunitJupiter}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.JunitJupiter}")
-}
 
 
 
 configurations.forEach { it.exclude("org.slf4j", "slf4j-api") }
-
-tasks.withType<Test> {
-    useJUnitPlatform {
-        includeEngines("junit-jupiter")
-    }
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-    reports.html.isEnabled = true
-}
-
-
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "io.provenance.model"
-            artifactId = "asset-specifications"
-            version = "$projectVersion"
-
-            from(components["java"])
-
-            pom {
-                name.set("Provenance Asset Specifications")
-                description.set("Asset/NFT scope specifications for Provenance Blockchain metadata module")
-                url.set("https://provenance.io")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("vwagner")
-                        name.set("Valerie Wagner")
-                        email.set("tech@figure.com")
-                    }
-                }
-
-                scm {
-                    connection.set("git@github.com:provenance-io/asset-specifications.git")
-                    developerConnection.set("git@github.com/provenance-io/asset-specifications.git")
-                    url.set("https://github.com/provenance-io/asset-specifications")
-                }
-            }
-        }
-    }
-    signing {
-        sign(publishing.publications["maven"])
-    }
-}
 
 
 nexusPublishing {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "asset-specifications"
+
+
+include(
+    "specs",
+    "util"
+)
